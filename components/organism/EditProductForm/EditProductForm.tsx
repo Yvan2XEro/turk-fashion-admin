e471c0a15fac87596891141f27271e537f25af58
Yvan2xEro <yvankanaj@gmail.com>
2023-11-19 19:14:20 +0100

    Traitement du formulaire, correction des bug, Mise en place du systeme de tags, reorganisation de l'actionsheet

--- components/organism/EditProductForm/EditProductForm.tsx
+++ components/organism/EditProductForm/EditProductForm.tsx
@@ -236,14 +236,3 @@ export default function EditProductForm({
     </div>
   );
 }
-const languages = [
-  { label: "English", value: "en" },
-  { label: "French", value: "fr" },
-  { label: "German", value: "de" },
-  { label: "Spanish", value: "es" },
-  { label: "Portuguese", value: "pt" },
-  { label: "Russian", value: "ru" },
-  { label: "Japanese", value: "ja" },
-  { label: "Korean", value: "ko" },
-  { label: "Chinese", value: "zh" },
-];
