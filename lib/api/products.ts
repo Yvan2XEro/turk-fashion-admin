45c44028fed3969f16746203e5ce67ac322da3fd
Yvan2xEro <yvankanaj@gmail.com>
2023-11-29 11:59:01 +0100

    Suppression des images par defauts

--- lib/api/products.ts
+++ lib/api/products.ts
@@ -10,8 +10,8 @@ const FilterSchema = z.object({
 export const productSchema = z.object({
     name: z.string().min(2).max(200),
     photoUrl: z.string()
-        /*  */
-        .default("https://firebasestorage.googleapis.com/v0/b/turkfasion.appspot.com/o/images%2Fad21d8a7-c775-4d70-ba66-bdc6979ebe0djeans%203.jpeg?alt=media&token=099a054f-c666-4e0d-888a-8dac5dac98f1")
+    /*  */
+
 
     ,
     subCategory: z.number(),
--- lib/api/sub-categories.ts
+++ lib/api/sub-categories.ts
@@ -9,8 +9,8 @@ export const subCategorySchema = z.object({
     category: z.number(),
     filters: z.array(z.number()).default([]),
     photoUrl: z.string()
-        /*  */
-        .default("https://firebasestorage.googleapis.com/v0/b/turkfasion.appspot.com/o/images%2Fad21d8a7-c775-4d70-ba66-bdc6979ebe0djeans%203.jpeg?alt=media&token=099a054f-c666-4e0d-888a-8dac5dac98f1"),
+    /*  */
+    ,
 })
 
 export type SubCategoryPayload = z.infer<typeof subCategorySchema>
